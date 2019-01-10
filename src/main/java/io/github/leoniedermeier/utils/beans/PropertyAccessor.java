8a0c4d9fe42b9e88037475975ef095c86ebc2705
LeoNiedermeier <LeoNiedermeier@users.noreply.github.com>
2019-01-10 22:04:47 +0100

    vereinfacht

--- src/main/java/io/github/leoniedermeier/utils/beans/PropertyAccessor.java
+++ src/main/java/io/github/leoniedermeier/utils/beans/PropertyAccessor.java
@@ -2,11 +2,7 @@ package io.github.leoniedermeier.utils.beans;
 
 import java.util.function.Function;
 
-/**
- * 
- * @param <T> The type of the bean.
- */
-public class PropertyAccessor<T> {
+public class PropertyAccessor {
 
 	public static <T, T1, T2, U> U get(T value, Function<? super T, ? extends T1> mapper1,
 			Function<? super T1, ? extends T2> mapper2, Function<? super T2, ? extends U> mapper3) {
@@ -19,35 +15,6 @@ public class PropertyAccessor<T> {
 	}
 
 	public static <T, U> U get(T value, Function<? super T, ? extends U> mapper1) {
-		return PropertyAccessor.of(value).then(mapper1).get();
-	}
-
-	public static <T> PropertyAccessor<T> of(T value) {
-		return new PropertyAccessor<>(value);
-	}
-
-	private final T value;
-
-	private PropertyAccessor(T value) {
-		this.value = value;
-	}
-
-	/**
-	 * For properties a returning {@code null} is nothing special.
-	 */
-	public T get() {
-		return value;
-	}
-
-	public T orElse(T other) {
-		return value != null ? value : other;
-	}
-
-	public <U> PropertyAccessor<U> then(Function<? super T, ? extends U> mapper) {
-		if (value == null) {
-			return new PropertyAccessor<>(null);
-		} else {
-			return new PropertyAccessor<>(mapper.apply(value));
-		}
+		return value == null ? null : mapper1.apply(value);
 	}
 }
--- src/test/java/io/github/leoniedermeier/utils/beans/PropertyAccessorTest.java
+++ src/test/java/io/github/leoniedermeier/utils/beans/PropertyAccessorTest.java
@@ -15,6 +15,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import io.github.leoniedermeier.utils.beans.PropertyAccessorTest.Address;
+
 class PropertyAccessorTest {
 
 	static class Address {
@@ -34,34 +36,37 @@ class PropertyAccessorTest {
 	}
 
 	@Nested
-	@DisplayName("Test for get()")
-	class TestGet {
+	@DisplayName("Test for get(T, Function)")
+	class TestGet1 {
 		@Test
 		@DisplayName("Existing property: value")
 		void property_get_existing() {
 			Person person = new Person();
-			Person result = PropertyAccessor.of(person).get();
+			person.address = new Address();
+			Address result = PropertyAccessor.get(person, Person::getAddress);
 
-			assertSame(person, result);
+			assertSame(person.address, result);
 		}
 
 		@Test
 		@DisplayName("Null property: returns null")
 		void property_get_null_throws_exception() {
-			assertNull(PropertyAccessor.of(null).get());
+			Person person = new Person();
+			Address result = PropertyAccessor.get(person, Person::getAddress);
+			assertNull(result);
 		}
 	}
 
 	@Nested
-	@DisplayName("Test for nested properties")
-	class TestNested {
+	@DisplayName("Test for get(T, Function, Function)")
+	class TestGet2 {
 
 		@Test
 		@DisplayName("Not existing nested properties")
 		void not_existing_nested_properties() {
 			Person person = new Person();
-			String result = PropertyAccessor.of(person).then(Person::getAddress).then(Address::getStreet).orElse("X");
-			assertSame("X", result);
+			String result = PropertyAccessor.get(person, Person::getAddress, Address::getStreet);
+			assertNull(result);
 		}
 
 		@Test
@@ -70,46 +75,10 @@ class PropertyAccessorTest {
 			Person person = new Person();
 			person.address = new Address();
 			person.address.street = "Street";
-			String result = PropertyAccessor.of(person).then(Person::getAddress).then(Address::getStreet).get();
-			assertSame(person.address.street, result);
-		}
-
-		@Test
-		@DisplayName("Existing nested properties with of")
-		void existing_nested_properties_with_of() {
-			Person person = new Person();
-			person.address = new Address();
-			person.address.street = "Street";
 			String result = PropertyAccessor.get(person, Person::getAddress, Address::getStreet);
 			assertSame(person.address.street, result);
 		}
+
 	}
 
-	@Nested
-	@DisplayName("Test for orElse()")
-	class TestOrElse {
-
-		@Test
-		@DisplayName("Existing property: value == 'property'")
-		void orElse_existing_property() {
-			Person person = new Person();
-			Object result = PropertyAccessor.of(person).orElse(new Person());
-			assertSame(person, result);
-		}
-
-		@Test
-		@DisplayName("Null property: value == 'other'")
-		void orElse_null_property() {
-
-			Object result = PropertyAccessor.of(null).orElse("X");
-			assertEquals("X", result);
-		}
-	}
-
-	@Test
-	@DisplayName("Factory Method: of")
-	void of() {
-		PropertyAccessor<?> propertyAccessor = PropertyAccessor.of(null);
-		assertNotNull(propertyAccessor, "PropertyAcessor must not be null");
-	}
 }
